b09cb33eefc8174a26333f27dea169d1dd3d060c
Kristian Skarseth <kristianskarseth@msn.com>
2013-05-14 20:35:45 +0200

    Fixed the required multiplication in 16x multisampling.
    Played around with the scene

--- Kristian Skarseth - Innlevering 3/src/RayTracer.cpp
+++ Kristian Skarseth - Innlevering 3/src/RayTracer.cpp
@@ -66,57 +66,10 @@ void RayTracer::render() {
 					std::cout << progress << "%" << std::endl;
 				}
 			}
-			//if(i > fb->getWidth()/2)
-			//	fb->setPixel(i,j,raytrace_64x_multisampled(i, j));
-			//else
 				fb->setPixel(i,j, raytrace_16x_multisampled(i, j));
-			
 		}
 	}
 	std::cout << "100%" << std::endl;
-	//For every pixel, ray-trace using multiple CPUs
-	//#ifdef _OPENMP
-	//#pragma omp parallel for
-	//	for (int j=0; j<static_cast<int>(fb->getHeight()); ++j) {
-	//#else
-	//	for (unsigned int j=0; j<fb->getHeight(); ++j) {
-	//#endif
-
-	/*std::vector<ScreenCoord> screen_coords;
-	screen_coords.resize(fb->getHeight()*fb->getWidth());
-	unsigned int counter = 0;
-	for (unsigned int j=0; j<fb->getHeight(); ++j) {
-	for (unsigned int i=0; i<fb->getWidth(); ++i) {
-	screen_coords.at(counter).x = j;
-	screen_coords.at(counter).y = i;
-	counter++;
-	}
-	}
-
-	unsigned int index_count = 0;
-	std::vector<std::shared_ptr<Thread>> threads; 
-	unsigned int threads_number = boost::thread::hardware_concurrency();
-	unsigned int offset = floor( (float)screen_coords.size() / (float)threads_number );
-
-	for(unsigned int i = 0; i < threads_number; ++i)
-	{
-	if(i == (threads_number-1) )
-	{
-	offset = (screen_coords.size()) - index_count;
-	}
-	std::shared_ptr<Thread> new_thread = std::make_shared<Thread>();
-	new_thread->thread.reset(new boost::thread(std::bind(&RayTracer::renderFrameArea, this, 
-	&screen_coords, index_count, index_count + offset, new_thread)));
-	new_thread->thread_id = i;
-	new_thread->thread_progress = 0.1f;
-	new_thread->next_progress_target = lerp(index_count, index_count+offset, new_thread->thread_progress);
-	threads.p	ush_back(new_thread);
-	index_count+=offset;
-	}
-
-	for(unsigned int i = 0; i < threads.size(); i++){
-	threads.at(i)->thread->join();
-	}*/
 }
 
 void RayTracer::save(std::string basename, std::string extension) {
@@ -158,32 +111,6 @@ void RayTracer::save(std::string basename, std::string extension) {
 	ilDeleteImages(1, &texid);
 }
 
-void RayTracer::renderFrameArea( std::vector<ScreenCoord>* screen_coords, 
-	unsigned int start_index, unsigned int end_index,
-	std::shared_ptr<Thread> thread_info)
-{
-	int workload = end_index-start_index;
-	for(unsigned int f = start_index; f < end_index; f++)
-	{
-		unsigned int i = screen_coords->at(f).y;
-		unsigned int j = screen_coords->at(f).x;
-		//glm::vec3 out_color(0.0, 0.0, 0.0);
-		//float t = std::numeric_limits<float>::max();
-
-
-		fb->setPixel(i, j, raytrace_64x_multisampled(i, j) );
-
-		if(f >= thread_info->next_progress_target)
-		{
-			std::cout<< "Thread " << thread_info->thread_id << ": " 
-				<< thread_info->thread_progress*100.0f << "%" << std::endl;
-			thread_info->thread_progress+=0.1f;
-			thread_info->next_progress_target = lerp(start_index, end_index, thread_info->thread_progress);
-		}
-	}
-	std::cout<< "Thread " << thread_info->thread_id << " done."<< std::endl;
-}
-
 float RayTracer::lerp( int i0, int i1, float t )
 {
 	float v0 = (float)i0;
@@ -193,11 +120,11 @@ float RayTracer::lerp( int i0, int i1, float t )
 }
 
 const glm::vec2 RayTracer::sample_16x_values[] = {
-	glm::vec2(-0.25, 0.25), glm::vec2(-0.125, 0.25),glm::vec2(0.125, 0.25),glm::vec2(0.25, 0.25),
-	glm::vec2(-0.25, 0.125), glm::vec2(-0.125, 0.125),glm::vec2(0.125, 0.125),glm::vec2(0.25, 0.125),
+	glm::vec2(-0.50, 0.50), glm::vec2(-0.25, 0.50),glm::vec2(0.125, 0.50),glm::vec2(0.50, 0.50),
+	glm::vec2(-0.50, 0.25), glm::vec2(-0.25, 0.25),glm::vec2(0.125, 0.125),glm::vec2(0.50, 0.125),
 
-	glm::vec2(-0.25, -0.125), glm::vec2(-0.125, -0.125),glm::vec2(0.125, -0.125),glm::vec2(0.25, -0.125),
-	glm::vec2(-0.25, -0.25), glm::vec2(-0.125, -0.25),glm::vec2(0.125, -0.25),glm::vec2(0.25, -0.25)
+	glm::vec2(-0.50, -0.25), glm::vec2(-0.25, -0.25),glm::vec2(0.25, -0.25),glm::vec2(0.50, -0.25),
+	glm::vec2(-0.50, -0.50), glm::vec2(-0.25, -0.50),glm::vec2(0.25, -0.50),glm::vec2(0.50, -0.50)
 };
 const std::size_t RayTracer::sample_16x_array_length = sizeof(sample_16x_values)/sizeof(sample_16x_values[0]);
 
@@ -240,7 +167,6 @@ glm::vec3 RayTracer::raytrace_4x_multisampled( unsigned int i, unsigned int j )
 	glm::vec3 d4 = glm::vec3(x, y, z);
 	Ray r4 = Ray(state->getCamPos(), d4);
 
-	//Now do the ray-tracing to shade the pixel
 	return 0.25f*(state->rayTrace(r1)+state->rayTrace(r2)+state->rayTrace(r3)+state->rayTrace(r4)) ;
 }
 
@@ -263,8 +189,8 @@ glm::vec3 RayTracer::raytrace_16x_multisampled( unsigned int i, unsigned int j )
 	glm::vec3 color(0.0f);
 
 	for(std::size_t t = 0; t < sample_16x_array_length; t++){
-		dir.x = (i+sample_16x_values[t].x*2)*(screen.right-screen.left)/static_cast<float>(fb->getWidth()) + screen.left;
-		dir.y = (j+sample_16x_values[t].y*2)*(screen.top-screen.bottom)/static_cast<float>(fb->getHeight()) + screen.bottom;
+		dir.x = (i+sample_16x_values[t].x)*(screen.right-screen.left)/static_cast<float>(fb->getWidth()) + screen.left;
+		dir.y = (j+sample_16x_values[t].y)*(screen.top-screen.bottom)/static_cast<float>(fb->getHeight()) + screen.bottom;
 		Ray ray = Ray(state->getCamPos(), dir);
 		color+=state->rayTrace(ray);
 	}
@@ -283,6 +209,5 @@ glm::vec3 RayTracer::raytrace_64x_multisampled( unsigned int i, unsigned int j )
 		Ray ray = Ray(state->getCamPos(), dir);
 		color+=state->rayTrace(ray);
 	}
-	//Now do the ray-tracing to shade the pixel
 	return (1.0f/sample_64x_array_length)*color;
 }
--- Kristian Skarseth - Innlevering 3/src/main.cpp
+++ Kristian Skarseth - Innlevering 3/src/main.cpp
@@ -30,8 +30,8 @@ int main(int argc, char *argv[]) {
 	try {
 		RayTracer* rt;
 		Timer t;
-		unsigned int width = 4000;
-		unsigned int height = 3000;
+		unsigned int width = 1920;
+		unsigned int height = 1080;
 		rt = new RayTracer(width, height);
 		float aspect = width/static_cast<float>(height);
 
@@ -40,7 +40,7 @@ int main(int argc, char *argv[]) {
 		std::shared_ptr<SceneObjectEffect> reflect(new ReflectEffect(glm::vec3(0.0, 0.0, 10.0)));
 		std::shared_ptr<SceneObjectEffect> phongshaded(new ShadedPhongEffect());
 
-		std::shared_ptr<SceneObjectEffect> fresnel(new FresnelEffect(Snell::Air, Snell::));
+		std::shared_ptr<SceneObjectEffect> fresnel(new FresnelEffect(Snell::Air, Snell::Glass));
 		std::shared_ptr<LightObject> light1(new PointLight(glm::vec3(-5.0f, 0.0f, 9.0f)));
 		rt->addLightSource(light1);
 		std::shared_ptr<LightObject> light2(new PointLight(glm::vec3(1.0f, 5.0f, 9.0f)));
@@ -60,30 +60,28 @@ int main(int argc, char *argv[]) {
 
 		std::shared_ptr<SceneObject> cubemap(new CubeMap(cubemap::SaintLazarusChurch));
 		rt->addSceneObject(cubemap);
-	/*	
+	
 		std::shared_ptr<SceneObject> myplane(new SizedPlane(
-			glm::vec3(-1.0f, -5.0f, 5.0f),
-			glm::vec3(8.0f, -5.0f, 5.0f),
-			glm::vec3(8.0f, -5.0f, -30.0f),
-			glm::vec3(-30.0f, -5.0f, -30.0f), reflect));
-		rt->addSceneObject(myplane);*/
+			glm::vec3(-1.0f, -10.0f, 5.0f),
+			glm::vec3(8.0f, -10.0f, 5.0f),
+			glm::vec3(8.0f, -10.0f, -30.0f),
+			glm::vec3(-30.0f, -10.0f, -30.0f), phongshaded));
+		rt->addSceneObject(myplane);
 
 
-	/*	std::shared_ptr<SceneObject> triangle(new Triangle(glm::vec3(-15.0, -10.0, -4.0), glm::vec3(15.0f, -10.0, -4),
-		glm::vec3(0, 10.0f, -15.0f), reflect));
-		rt->addSceneObject(triangle);*/
+		std::shared_ptr<SceneObject> triangle(new Triangle(glm::vec3(0.0, -10.0, -4.0), glm::vec3(30.0f, -10.0, -4),
+		glm::vec3(15, 10.0f, -15.0f), reflect));
+		rt->addSceneObject(triangle);
 		
 		std::shared_ptr<SceneObject> s1(new Sphere(glm::vec3(0.0f, 0.0f, 0.0f), 2.0f, fresnel));
 		rt->addSceneObject(s1);
 		std::shared_ptr<SceneObject> s2(new Sphere(glm::vec3(-8.0f, 0.0f, 0.0f), 2.0f, reflect));
 		rt->addSceneObject(s2);
-		/*std::shared_ptr<SceneObject> s2(new Sphere(glm::vec3(1.0f, 0.0f, 2.0f), 2.0f, reflect));
-		rt->addSceneObject(s2);*/
-		//std::shared_ptr<SceneObject> s3(new Sphere(glm::vec3(0.0f, 3.0f, 0.0f), 2.0f, fresnel));
-		//rt->addSceneObject(s3);
+		//std::shared_ptr<SceneObject> s2(new Sphere(glm::vec3(1.0f, 0.0f, 2.0f), 2.0f, reflect));
+		//rt->addSceneObject(s2);
 
-		//std::shared_ptr<SceneObject> s4(new Sphere(glm::vec3(-7.0f, -4.0f, 0.0f), 2.0f, fresnel));
-		//rt->addSceneObject(s4);
+		std::shared_ptr<SceneObject> s4(new Sphere(glm::vec3(-7.0f, -7.0f, 0.0f), 2.0f, phongshaded));
+		rt->addSceneObject(s4);
 		std::shared_ptr<SceneObject> s5(new Sphere(glm::vec3(6.0f, 1.0f, 0.0f), 2.0f, fresnel));
 		rt->addSceneObject(s5);
 		
